96151e07e575bf9d49a80d3ca16de155752734f1
Gürkan Gökdemir <gurkangokdemir@outlook.com>
2019-04-25 10:38:16 +0300

    Update Builder.cs

--- Builder/Builder.cs
+++ Builder/Builder.cs
@@ -1,1 +1,25 @@
-Builder 
+@startuml
+class CartBuilderPatternDemo{
++main(): void
+}
+
+abstract class CartBuilder{
++takeTinyCart: Cart
++takeGoldCart: Cart
+}
+
+class Cart{
+-items:ArrayList<item>
++addItem(Item item): void
++showItems(): void
++getCost(): float
+}
+
+class Item{
++name(): String
++price(): float
++packing(): Packing
+}
+
+CartBuilder <-- CartBuilderPatternDemo: asks
+@enduml
