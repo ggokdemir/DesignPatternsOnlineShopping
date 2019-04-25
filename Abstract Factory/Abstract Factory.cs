17ef5fde3b93dcce824503917bdcfc8ad5ccb71d
Gürkan Gökdemir <gurkangokdemir@outlook.com>
2019-04-25 10:17:11 +0300

    Update Abstract Factory.cs

--- Abstract Factory/Abstract Factory.cs
+++ Abstract Factory/Abstract Factory.cs
@@ -1,1 +1,29 @@
-Abstract Factory
+@startuml
+class CustomerDemo{
++main(): void
+}
+
+class CustomerProducer{
++getFactory(): CustomerFactory
+}
+
+class CustomerFactory{
++getCustomerInfo():Info
+}
+
+class InfoFactory{
++getCustomerInfo(): Info
+}
+
+class CustomerInfo{
++getName(): String
++getSurnam(): String
++getAdress(): String
++getId(): String
++getCardId() : String
++getPassword() : String 
+}
+
+CustomerFactory <-- CustomerProducer :uses
+CustomerProducer <-- CustomerDemo :uses
+@enduml
