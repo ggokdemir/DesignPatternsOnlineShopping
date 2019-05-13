d8e87d3f811e7464ab922931302a7252ca775fec
Gürkan Gökdemir <25768163+gurkangokdemir@users.noreply.github.com>
2019-05-13 15:49:22 +0300

    Update Singleton.cs

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -1,1 +1,75 @@
-Singleton
+                 
+
+using System;
+
+ 
+
+namespace gurkangokdemir
+{
+
+  class MainApp
+
+  {
+
+    static void Main()
+
+    {
+
+      Singleton s1 = Singleton.Instance();
+
+      Singleton s2 = Singleton.Instance();
+
+      if (s1 == s2)
+
+      {
+
+        Console.WriteLine("Objects are the same instance");
+
+      }
+
+      Console.ReadKey();
+
+    }
+
+  }
+
+
+
+  class Singleton
+
+  {
+
+    private static Singleton _instance;
+
+
+
+    protected Singleton()
+
+    {
+
+    }
+
+ 
+
+    public static Singleton Instance()
+
+    {
+
+      if (_instance == null)
+
+      {
+
+        _instance = new Singleton();
+
+      }
+
+ 
+
+      return _instance;
+
+    }
+
+  }
+
+}
+ 
