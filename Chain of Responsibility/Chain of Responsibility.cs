f821e526da76563c2597a19b5733f8a6168101e2
Gürkan Gökdemir <25768163+gurkangokdemir@users.noreply.github.com>
2019-05-13 15:19:06 +0300

    Update Chain of Responsibility.cs

--- Chain of Responsibility/Chain of Responsibility.cs
+++ Chain of Responsibility/Chain of Responsibility.cs
@@ -1,1 +1,15 @@
-Chain of Responsibility
+
+using System;
+namespace Chain_of_Responsibility
+{
+    class Chain_of_Responsibility 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Chain of Responsibility");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
