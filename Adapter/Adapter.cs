7ae5dd0a7673f1731db666b26390576f8659c488
Gürkan Gökdemir <25768163+gurkangokdemir@users.noreply.github.com>
2019-05-13 15:25:50 +0300

    Update Adapter.cs

--- Adapter/Adapter.cs
+++ Adapter/Adapter.cs
@@ -1,1 +1,82 @@
-Adapter
+using System;
+
+ 
+
+namespace gurkangokdemir
+
+{
+
+  class MainApp
+
+  {
+
+
+    static void Main()
+
+    {
+
+      Target target = new Adapter();
+
+      target.Request();
+
+
+      Console.ReadKey();
+
+    }
+
+  }
+
+ 
+  class Target
+
+  {
+
+    public virtual void Request()
+
+    {
+
+      Console.WriteLine("Called Target Request()");
+
+    }
+
+  }
+
+
+  class Adapter : Target
+
+  {
+
+    private Adaptee _adaptee = new Adaptee();
+
+ 
+
+    public override void Request()
+
+    {
+
+
+      _adaptee.SpecificRequest();
+
+    }
+
+  }
+
+ 
+
+  class Adaptee
+
+  {
+
+    public void SpecificRequest()
+
+    {
+
+      Console.WriteLine("Called SpecificRequest()");
+
+    }
+
+  }
+
+}
+ 
+ 
